5d4435af7a562251067c0bd6d723fba54f4c02e2
Rob Clarke <robert.clarke@bristol.ac.uk>
2021-10-20 14:02:17 +0100

    Add scripting to fix default materials

--- fenswood/setup.bash
+++ fenswood/setup.bash
@@ -3,3 +3,8 @@ export GAZEBO_RESOURCE_PATH=/ros.env.d/fenswood/worlds:${GAZEBO_RESOURCE_PATH}
 
 # Add the 'grass_box' model to the Gazebo model path
 export GAZEBO_MODEL_PATH=/ros.env.d/fenswood/models:${GAZEBO_MODEL_PATH}
+
+# Make the default media files work
+cp -r /usr/share/gazebo-11/media /root/gzweb/http/client/assets/
+(cd /root/gzweb/http/client/assets/media/materials/textures \
+    && for f in *jpg; do convert $f ${f%.*}.png; done)
