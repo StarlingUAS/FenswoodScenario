2148bc3ffe213f1f7a74f6498c44d03e7660ace7
Rob Clarke <robert.clarke@bristol.ac.uk>
2021-10-20 13:56:23 +0100

    Initial version

---
+++ fenswood/setup.bash
@@ -0,0 +1,5 @@
+# Add fenswood.world to the Gazebo resource path
+export GAZEBO_RESOURCE_PATH=/ros.env.d/fenswood/worlds:${GAZEBO_RESOURCE_PATH}
+
+# Add the 'grass_box' model to the Gazebo model path
+export GAZEBO_MODEL_PATH=/ros.env.d/fenswood/models:${GAZEBO_MODEL_PATH}
