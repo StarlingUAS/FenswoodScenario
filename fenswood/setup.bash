56b9b2556d2ab39c463eea84d8e0bccc1f9a1673
mickey li <mickeyhli@outlook.com>
2021-11-01 13:51:26 +0000

    Added area-of-interest model

--- fenswood/setup.bash
+++ fenswood/setup.bash
@@ -1,10 +1,14 @@
+SRC_PATH=/ros.env.d/fenswood
+
 # Add fenswood.world to the Gazebo resource path
-export GAZEBO_RESOURCE_PATH=/ros.env.d/fenswood/worlds:${GAZEBO_RESOURCE_PATH}
+export GAZEBO_RESOURCE_PATH=$SRC_PATH/worlds:${GAZEBO_RESOURCE_PATH}
+echo "RESOURCE_PATH: $GAZEBO_RESOURCE_PATH"
 
 # Add the 'grass_box' model to the Gazebo model path
-export GAZEBO_MODEL_PATH=/ros.env.d/fenswood/models:${GAZEBO_MODEL_PATH}
+export GAZEBO_MODEL_PATH=$SRC_PATH/models:${GAZEBO_MODEL_PATH}
+echo "GAZEBO_MODEL_PATH: $GAZEBO_MODEL_PATH"
 
 # Make the default media files work
-cp -r /usr/share/gazebo-11/media /root/gzweb/http/client/assets/
+cp -r /usr/share/gazebo-11/media $GZWEB_WS/http/client/assets/
 (cd /root/gzweb/http/client/assets/media/materials/textures \
     && for f in *jpg; do convert $f ${f%.*}.png; done)
